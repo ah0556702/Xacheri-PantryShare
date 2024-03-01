5df10fca2d0c0809b8c0db1d6049a58c59aaf3a8
ah0556702 <ah0556702@otc.edu>
2024-03-01 09:43:37 -0600

    Init commit

---
+++ View/landingPage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="landing" id="landing">
+        <a href="#landingPage">
+            <img src="images/logo.png" alt="logoIcon">
+        </a>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include "View/landingPage.php";
+    ?>
+        /*switch($action){
+            case "register":
+                //$username = $_POST['username'];
+                //$fName = $_POST['fName'];
+                //$lName = $_POST['lName'];
+                //$email = $_POST['email'];
+
+            case "login":
+                //$username = $_POST['username'];
+                //$password = $_POST['password'];
+
+            case "make_request":
+                include "View/requestForm.php";
+
+            case "fulfill_request":
+                // redirect to the requests detail page
+                // with checkbox values in list form submitted
+                // by user in request
+            
+            case "communicate": // this should just show notification messages such as.. "Your request has been deliverd" or "Thank you for fulfilling a request"
+                include "View/Messages.php";
+        }*/
+
+    ?>
+</body>
+</html>
